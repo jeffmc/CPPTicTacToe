09b779d75207241ee86da58af00aaca35664a97b
jeffmc <10823453+jeffmc@users.noreply.github.com>
2022-09-18 23:45:21 -0700

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@
 
 using namespace std;
 
-// Hopefully this could be better implemented in the future.
-#define BOARD_SZ 3
+#define BOARD_SZ 10
 #define BOARD_SQ BOARD_SZ*BOARD_SZ
+
+#define ALPHABET_STR       "abcdefghijklmnopqrstuvwxyz"
+#define ALPHABET_STR_UPPER "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+#if BOARD_SZ>26
+	#error BOARD_SZ cannot be more than 26.
+#endif
 
 class TTTState {
 	public:
 		unsigned char board[BOARD_SQ]; // 0 for empty, 1 for X, 2 for O.
-	public:
+		public:
 		TTTState();
 		bool gameFinished(); // returns true if no more moves can be made.
 		unsigned char gameWinner(); // returns 1 for X win, 2 for O win, 0 for cat game, should return -1 if game not over.
@@ -23,10 +29,22 @@ TTTState::TTTState() {
 	for (int i=0;i<BOARD_SQ;i++) board[i] = 0;
 }
 
+// Override ostream << operator with a TTTState arg in order to easily print to std::cout.
 ostream& operator<<(ostream &out, TTTState ttt) {
-	out << "  a b c" << endl;
+	const char* alpha = ALPHABET_STR;
+
+	const int rowTitleWidth = 4; // Should be determined based upon BOARD_SZ ceil(log(BOARD_SZ,10));
+	out.fill(' ');
+	out.width(rowTitleWidth+1);
+	out << ' ';
+	for (int i=0;i<BOARD_SZ;i++) out << alpha[i] << ' ';
+	out << endl;
+	
 	for (int j=0;j<BOARD_SZ;j++) {
-		out << (j+1) << ' ';
+		out.width(4);
+		out << right <<	(j+1) << ' ';
+		
+		out.width(0);
 		for (int i=0;i<BOARD_SZ;i++) {
 			unsigned char s = ttt.board[j*BOARD_SZ+i];
 			switch (s) {
@@ -48,11 +66,28 @@ ostream& operator<<(ostream &out, TTTState ttt) {
 	return out;
 };
 
-
 int main() {
 	
-	TTTState *ttt = new TTTState();
-	cout << *ttt;
+	unsigned char turn = 1; // X always starts
+
+	bool playing = true;
+	char simp;
+	while (playing) {
+		cout << endl << (turn == 1 ? 'X' : 'O') << "'s turn!" << endl;
+
+		TTTState *ttt = new TTTState();
+		cout << *ttt;
+
+		cin >> simp;
+		
+		if (turn == 1) {
+			turn = 2;
+		} else {
+			turn = 1;
+		}
+
+	}
+
 
 	return 0;
 
