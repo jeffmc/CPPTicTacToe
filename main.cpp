abdee4c444a9be05c5c6f24ab7c47c8a73395e67
Jeff McMillan <10823453+jeffmc@users.noreply.github.com>
2022-09-20 14:19:17 -0700

    modified:   main.cpp 	new file:   pseudocode.txt

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@
 
 using namespace std;
 
-#define BOARD_SZ 10
+#define BOARD_SZ 3 
 #define BOARD_SQ BOARD_SZ*BOARD_SZ
 
-#define ALPHABET_STR       "abcdefghijklmnopqrstuvwxyz"
-#define ALPHABET_STR_UPPER "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define ALPHABET_STR "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 #if BOARD_SZ>26
 	#error BOARD_SZ cannot be more than 26.
@@ -17,20 +16,49 @@ using namespace std;
 
 class TTTState {
 	public:
-		unsigned char board[BOARD_SQ]; // 0 for empty, 1 for X, 2 for O.
+		unsigned char board[BOARD_SQ]; // 0b100 for empty, 0b010 for X, 0b001 for O.
 		public:
 		TTTState();
-		bool gameFinished(); // returns true if no more moves can be made.
-		unsigned char gameWinner(); // returns 1 for X win, 2 for O win, 0 for cat game, should return -1 if game not over.
+		bool finished(); // returns true if no more moves can be made.
+		unsigned char winner(); // returns 0b100 for CAT, 0b010 for X win, 0b001 O win, should return 0b000 if game not over.
 		bool set(int x, int y, unsigned char p); // returns true if space was available and set, false if already occupied.
+	private:
+		void checkSets(unsigned char* sf, int* sfb, int boff, int bStr, int cStr, int nSets);
 };
 
 TTTState::TTTState() {
-	for (int i=0;i<BOARD_SQ;i++) board[i] = 0;
+	for (int i=0;i<BOARD_SQ;i++) board[i] = 0b100;
+}
+
+unsigned char TTTState::winner() {
+	unsigned char sf[(BOARD_SZ+1)*2] = { };
+	int sfi = 0;
+	checkSets(sf,&sfi,0,BOARD_SZ,1,BOARD_SZ); // rows
+	checkSets(sf,&sfi,0,1,BOARD_SZ,BOARD_SZ); // cols
+	checkSets(sf,&sfi,0,0,BOARD_SZ+1,1); // top left
+	checkSets(sf,&sfi,BOARD_SZ-1,0,BOARD_SZ-1,1); // top right
+
+	return -1; // TODO: Fix
+}
+
+void TTTState::checkSets(unsigned char* sf, int* sfb, int boff, int bStr, int cStr, int nSets) {
+	int b = boff; // beginning.
+	for (int n=0;n<nSets;n++) {
+		int c = b;	
+		for (int i=0;i<BOARD_SZ;i++) {
+			// cout << c << ' ';
+			sf[*sfb+n] |= this->board[c];
+			c += cStr;	
+		}
+		// cout << endl;
+		b += bStr;
+	}
+	*sfb+=nSets;
+
 }
 
 // Override ostream << operator with a TTTState arg in order to easily print to std::cout.
-ostream& operator<<(ostream &out, TTTState ttt) {
+ostream& operator<<(ostream &out, TTTState* ttt) {
 	const char* alpha = ALPHABET_STR;
 
 	const int rowTitleWidth = 4; // Should be determined based upon BOARD_SZ ceil(log(BOARD_SZ,10));
@@ -46,16 +74,17 @@ ostream& operator<<(ostream &out, TTTState ttt) {
 		
 		out.width(0);
 		for (int i=0;i<BOARD_SZ;i++) {
-			unsigned char s = ttt.board[j*BOARD_SZ+i];
+			unsigned char s = ttt->board[j*BOARD_SZ+i];
 			switch (s) {
-				case 0:
+				case 0b100:
 					out << '_';
 					break;
-				case 1:
+				case 0b010:
 					out << 'X';
 					break;
-				case 2:
+				case 0b001:
 					out << 'O';
+					break;
 				default:
 					out << "GARBLED DATA!!!!";
 			}
@@ -76,7 +105,23 @@ int main() {
 		cout << endl << (turn == 1 ? 'X' : 'O') << "'s turn!" << endl;
 
 		TTTState *ttt = new TTTState();
-		cout << *ttt;
+		cout << ttt;
+
+		int res = ttt->winner();
+		switch (res) {
+			case 0b100:
+				cout << "Still playing...";
+				break;
+			case 0b010:
+				cout << "X won!";
+				break;
+			case 0b001:
+				cout << "O won!";
+				break;
+			default:
+				cout << "GARBLED RESULT!";
+				break;
+		}
 
 		cin >> simp;
 		
@@ -92,5 +137,3 @@ int main() {
 	return 0;
 
 };
-
-
